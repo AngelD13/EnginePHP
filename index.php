48e104d2fcd0d98891f8ee8a4cb0b69d3018d0e6
Korotin Evgeniy <35030244+AngelD13@users.noreply.github.com>
2020-03-27 11:33:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Движок на PHP
+ * @author Angel
+ */
+include_once "class/Engine.php"; //Подключаем класс-движка
+$engine = new Engine(); //Создаем объект класса Engine
+
+include_once "templates/main/header.php"; //Подключаем шапку сайта
+
+if ($engine->getError()) { //Если возникли ошибки, выводим сообщение на экран
+    echo "<div style='border:1px solid red;padding:10px;margin: 10px auto; 
+        width: 500px;'>" . $engine->getError() . "</div>";
+}
+echo $engine->getContentPage(); //Выводим страницы сайта
+
+include_once "templates/main/footer.php";//Подключаем подвал сайта
+?>
