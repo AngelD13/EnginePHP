5424903f184c79207e8a197df239a5442b6f2cf1
Korotin Evgeniy <35030244+AngelD13@users.noreply.github.com>
2020-03-28 07:43:00 +0200

    Шапка сайта

---
+++ templates/main/header.php
@@ -0,0 +1,22 @@
+<html>
+       <head>
+           <?php global $engine; ?>
+           <title><?= $engine->getTitle() ?></title>
+           <meta charset="utf-8" />
+            <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script><![endif]-->
+            <meta name="keywords" content="" />
+            <meta name="description" content="" />
+            <link href="CSS/style.css" rel="stylesheet">
+       </head>
+       <body>
+       <div class="wrapper">
+
+            <header class="header">
+                <strong>Новый Мир </strong>- информационный портал        
+            </header><!-- .header-->
+
+            <div class="headerMain">
+                    <a href="?page=main">Главная</a> | <a href="?page=about">О компании</a> | 
+                    <a href="?page=news">Новости</a> | <a href="?page=polls">Опрос</a> | 
+                    <a href="?page=asdasd">Битый раздел</a> | <a href="?page=login">Вход</a>
+            </div>
