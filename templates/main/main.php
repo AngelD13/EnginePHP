884afc0caff733b648be47cd5aeb298948af7ed7
Korotin Evgeniy <35030244+AngelD13@users.noreply.github.com>
2020-03-28 07:44:56 +0200

    Главная страница

---
+++ templates/main/main.php
@@ -0,0 +1,16 @@
+<div class="middle">
+
+		<div class="container">
+			<main class="content">
+				<strong>Тело:</strong> Здесь будет выполнение скрипта<br />
+                <h1>Главная страница сайта Нового Мира</h1>
+                <p>Сайт в разработке</p>
+				
+			</main><!-- .content -->
+		</div><!-- .container-->
+
+		<aside class="right-sidebar">
+			<strong>Правая колонка:</strong> Дополнительная информация
+		</aside><!-- .right-sidebar -->
+
+	</div><!-- .middle-->
